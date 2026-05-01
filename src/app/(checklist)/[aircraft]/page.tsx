e218fee81beb4418784e89eb7961d3fd06a11c4d
Jakub Hosak <hostacny@gmail.com>
2026-05-01 11:10:54 +0200

    Bump version to v3.0.11

--- src/app/(checklist)/[aircraft]/page.tsx
+++ src/app/(checklist)/[aircraft]/page.tsx
@@ -184,7 +184,7 @@ function getOriginalHTML(aircraft: string): string {
     </div>
   </div>
   <div class="global-footer">
-    <span class="version-info">v3.0.10</span>
+    <span class="version-info">v3.0.11</span>
     <span class="sim-warning">For flight simulation use only.<br>Not for real-world flight.</span>
   </div>`;
 }
